a67ed995218e6c78f18225334421ca385c3ca174
René Faßbender <rene-fassb@gmx.de>
2023-06-05 15:50:56 +0200

    feat: reduce page amount by dynamic handle

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function HomePage() {
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { introduction, volumes } from "../resources/lib/data";
+
+function getRandomElement(array) {
+  const randomValue = array[Math.floor(Math.random() * array.length)];
+  console.log(randomValue);
+  return randomValue;
+}
+
+export default function IndexPage() {
+  const router = useRouter();
+
+  const handleRandomVolume = () => {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  };
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleRandomVolume}>Random Volume</button>
     </div>
   );
 }
---
+++ pages/volumes/[slug].js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { volumes } from "../../resources/lib/data";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const volume = volumes.find(({ slug: volumeSlug }) => volumeSlug === slug);
+  const currentIndex = volumes.findIndex(
+    ({ slug: volumeSlug }) => volumeSlug === slug
+  );
+
+  if (!volume) {
+    return <div>Volume not found.</div>;
+  }
+
+  const previousVolume = volumes[currentIndex - 1];
+  const nextVolume = volumes[currentIndex + 1];
+
+  return (
+    <div>
+      <Link href="/">← All Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book) => (
+          <li key={book.slug}>{book.title}</li>
+        ))}
+      </ul>
+      <Image
+        src={volume.cover}
+        alt={volume.title}
+        width={140}
+        height={230}
+      ></Image>
+      {previousVolume && (
+        <Link href={`/volumes/${previousVolume.slug}`}>Previous Volume</Link>
+      )}
+      {nextVolume && (
+        <Link href={`/volumes/${nextVolume.slug}`}>Next Volume</Link>
+      )}
+    </div>
+  );
+}
